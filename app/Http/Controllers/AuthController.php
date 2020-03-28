2fdae055d87cc2d050520181353a6fd6d917bad3
unknown <murithipaul1@gmail.com>
2020-03-28 12:30:46 -0700

    implemented login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,5 +6,27 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request){
+        $http = new \GuzzleHttp\client;
+        try{
+            $response = $http->post('http://127.0.0.1:8000/oauth/token'), [
+                'form_params'=>[
+                    'grant_type'=>'password',
+                    'client_id'=> 2,
+                    'client_secret'=> '',
+                    'username'=>$request->username,
+                    'password'=>$request->password
+                ]
+            ]);
+            return $response->getBody();
+        }catch(\GuzzleHttp\Exception\BadResponseException $e){
+            if($e->getCode() = 400){
+                return response()->json('Invalid Request. Please enter username and password', $e->getCode());
+            }else if($e->getCode() = 401){
+                return response()->json('Icorrect credentials. Please try again', $e->getCode());
+            }else{
+                return response()->json('Something went wrong on the server', $e->getCode());
+            }
+        }
+    }
 }
