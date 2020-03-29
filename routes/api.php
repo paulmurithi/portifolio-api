710b7a65db3bf512279e73208d87a8dda7dadc13
unknown <murithipaul1@gmail.com>
2020-03-29 15:37:45 -0700

    edited routes

--- routes/api.php
+++ routes/api.php
@@ -16,8 +16,8 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::post('register', 'AuthController@register');
-Route::post('login', 'AuthController@login');
+Route::post('/register', 'AuthController@register');
+Route::post('/login', 'AuthController@login');
 Route::middleware('auth:api')->post('/logout', 'AuthController@logout');
 
 Route::ApiResource('projects', 'ProjectController');
